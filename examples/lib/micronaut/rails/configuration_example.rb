eefc3623976512f0c1fc7ed62746f4d671d4ada3
Rob Sanheim <rsanheim@gmail.com>
2008-12-29 13:22:36 -0500

    update for latest module accessors

--- examples/lib/micronaut/rails/configuration_example.rb
+++ examples/lib/micronaut/rails/configuration_example.rb
@@ -23,20 +23,20 @@ describe Micronaut::Rails::Configuration do
     method_to_modules.each do |method, mod| 
       example "##{method} with no filter options" do
         Micronaut.configuration.send(method)
-        Micronaut.configuration.extra_modules.should include([:extend, mod, {}])
+        Micronaut.configuration.include_or_extend_modules.should include([:extend, mod, {}])
       end
       
       example "##{method} with filter options" do
         filter_options = {:options => { "foo" => "bar" } }
         Micronaut.configuration.send(method, filter_options)
-        Micronaut.configuration.extra_modules.should include([:extend, mod, filter_options])
+        Micronaut.configuration.include_or_extend_modules.should include([:extend, mod, filter_options])
       end
     end
     
     example "#enable_rails_specific_mocking_extensions for mocha with no filter options" do
       Micronaut.configuration.mock_with :mocha
       Micronaut.configuration.enable_rails_specific_mocking_extensions
-      Micronaut.configuration.extra_modules.should include([:include, Micronaut::Rails::Mocking::WithMocha, {}])
+      Micronaut.configuration.include_or_extend_modules.should include([:include, Micronaut::Rails::Mocking::WithMocha, {}])
     end
 
   end
